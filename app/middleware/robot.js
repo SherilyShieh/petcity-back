f3cf68aef15ff529ac6692d580079995713643ae
sherily <1252524051@qq.com>
2021-02-01 15:45:01 +1300

    Transfer-Encoding

--- app/middleware/robot.js
+++ app/middleware/robot.js
@@ -2,6 +2,7 @@
 module.exports = (options, app) => {
     return async function robotMiddleware(ctx, next) {
         ctx.response.type = 'application/json';
+        ctx.set("Transfer-Encoding", "chunked");
         const source = ctx.get('user-agent') || '';
         const match = options.ua.some(ua => ua.test(source));
         if (match) {
